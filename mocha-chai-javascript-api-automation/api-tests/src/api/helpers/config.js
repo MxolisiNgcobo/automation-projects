ed93a05bee67be0ad2828be4a31d80076c697859
Mxolisi Ngcobo <mxolise@gmail.com>
2022-08-03 16:22:03 +0200

    cleanup

--- mocha-chai-javascript-api-automation/api-tests/src/api/helpers/config.js
+++ mocha-chai-javascript-api-automation/api-tests/src/api/helpers/config.js
@@ -2,10 +2,10 @@ export const apiUser = Role('https://any-api.com/ebay_com', async t => {
    await ussdSmartLogin(t);
 });
 
-export const USSD_SMART_MEMBER = '09876543' 
+export const USSD_SMART_MEMBER = '27646559471' 
 export const USSD_SMART_STRING = '**123**' 
 
 export const data_ussdSmartLogin = {
-    'password': "QATesting321", 
-    'username': "qatest321"
+    'password': "****", 
+    'username': "****"
 };
